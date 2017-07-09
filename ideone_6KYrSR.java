7f167a1afe18529002f61e47d624e5bcbbae8b4a
Darshonik <abhi1561995@gmail.com>
2017-07-09 12:10:07 +0530

    Add files via upload

---
+++ ideone_6KYrSR.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone_abhishek2
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner (System.in);
+	    System.out.println("enter the number");
+        int num = sc.nextInt();
+                if(num<30000 && num>(-30000))
+			{
+            if ((num%2)==0)
+            System.out.println("the number is even");
+            else
+            System.out.println("the number is odd");
+                        }
+        else
+			{
+				System.out.println("enter a number with smaller magnitude");
+			}
+	}
+}
