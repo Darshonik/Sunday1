ba262c5cc2ed1b4e75edb07091ebd7fd59918d00
Darshonik <abhi1561995@gmail.com>
2017-07-09 16:27:52 +0530

    Add files via upload

---
+++ ideone_hKUq55.java
@@ -0,0 +1,34 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int temp;
+	boolean isPrime=true;
+	Scanner scan= new Scanner(System.in);
+	System.out.println("Enter a number for check:");
+	//capture the input in an integer
+	int num=scan.nextInt();
+	for(int i=2;i<=num/2;i++)
+	{
+           temp=num%i;
+	   if(temp==0)
+	   {
+	      isPrime=false;
+	      break;
+	   }
+	}
+	//If isPrime is true then the number is prime else not
+	if(isPrime)
+	   System.out.println(num + " is Prime Number");
+	else
+	   System.out.println(num + " is not Prime Number");
+	}
+}
