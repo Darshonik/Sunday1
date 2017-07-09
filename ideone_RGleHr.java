65d1b76440b83db3efafc7c63852d5d9679f7acf
Darshonik <abhi1561995@gmail.com>
2017-07-09 11:38:04 +0530

    Add files via upload

---
+++ ideone_RGleHr.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+			
+			Scanner sc = new Scanner (System.in);
+			System.out.println("enter the number");
+			int num = sc.nextInt();
+			if(num<30000 && num>(-30000))
+			{
+            if(num==0)
+            System.out.println("the number is zero");
+            else if(num>0)
+            System.out.println("the number is positive");
+            else
+            System.out.println("the number is negative");
+			}
+			else
+			{
+				System.out.println("enter a number with smaller magnitude");
+			}
+	
+	}
+}
