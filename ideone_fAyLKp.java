32c80d72865d3e8fb64f9d68fd81537b4c3adcb6
Darshonik <abhi1561995@gmail.com>
2017-07-09 14:21:28 +0530

    Add files via upload

---
+++ ideone_fAyLKp.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone_ABHISHEK3
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner (System.in);
+	    System.out.println("enter the CHARACTER");
+        String ch = sc.next();
+        char c=ch.charAt(0);
+        
+            if ((c=='a')||(c=='e')||(c=='i')||(c=='o')||(c=='u')||(c=='A')||(c=='E')||(c=='I')||(c=='O')||(c=='U'))
+            System.out.println("IT IS A VOWEL");
+            else
+            System.out.println("IT IS A CONSONANT");
+                        }
+        
+        	}
